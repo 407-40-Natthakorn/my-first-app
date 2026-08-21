e180e5889b09ba2b5d50c35f73b534f0f0e4cbfd
407-40-Natthakorn <56714@yupparaj.ac.th>
2026-08-21 11:30:50 +0700

    Create vocab_game.py

---
+++ vocab_game.py
@@ -0,0 +1,118 @@
+import time
+import streamlit as st
+
+st.title("⏱️ เกมเติมศัพท์จับเวลา")
+
+if "ans1_val" not in st.session_state:
+    st.session_state.ans1_val = ""
+if "ans2_val" not in st.session_state:
+    st.session_state.ans2_val = ""
+if "ans3_val" not in st.session_state:
+    st.session_state.ans1_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans2_val = ""
+
+def reset_game():
+    st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
+    st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
+    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
+    st.session_state.start = time.time()  # เริ่มเวลาใหม่
+    st.session_state.is_ended = False  # ปิด Dialog
+
+@st.dialog("📊 สรุปผลการเล่นเกม")
+def show_result_dialog(ans1, ans2,ans3,ans4):
+    st.balloons()
+    score = 0
+
+    u_ans1 = ans1.strip().lower()
+    u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
+  
+    
+    if u_ans1 == "apple":
+        st.success("✅ ข้อ 1: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+
+    
+    if u_ans2 == "fish":
+        st.success("✅ ข้อ 2: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+
+  
+    if u_ans3 == "Gift":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+
+    
+    if u_ans4 == "Ballon":
+        st.success("✅ ข้อ 4: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+
+    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
+
+    if score == 4:
+        st.success("🎉 You win!")
+    else:
+        st.error("💀 You lose!")
+
+st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
+
+
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    time_left = int(60 - (time.time() - st.session_state.start))
+
+    if time_left > 0:
+        st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
+    else:
+        st.session_state.is_ended = True
+        st.rerun()
+
+st.divider()
+
+
+ans1 = st.text_input(
+    "ข้อ 1: A `B a _ _ n a` are a favorite food of monkeys. 🍌",
+    value=st.session_state.ans1_val,
+)
+ans2 = st.text_input(
+    "ข้อ 2: `L _ m _ n` have a sour taste. 🍋",
+    value=st.session_state.ans2_val,
+)
+ans3 = st.text_input(
+    "ข้อ 3: A `G i _ _` is something given on a birthday. 🎁",
+    value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+    "ข้อ 4: A `B a _ l _ o _` are commonly used to decorate birthday parties and festive events. 🎈",
+    value=st.session_state.ans4_val,
+)
+
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
+
+
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    if st.button("📥 ส่งคำตอบ"):
+        st.session_state.is_ended = True
+        st.rerun()
+
+    time.sleep(1)
+    st.rerun()
+
+if st.session_state.get("is_ended", False):
+    show_result_dialog(ans1, ans2,ans3,ans4)
+
+st.divider()
+st.write("นายณัฐกรณ์ อุ่นเรือน 40 4/7")
