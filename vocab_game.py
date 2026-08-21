026dd6b4a8fcce9af5370f834b08950107282653
407-40-Natthakorn <56714@yupparaj.ac.th>
2026-08-21 11:40:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,7 +52,7 @@ def show_result_dialog(ans1, ans2,ans3,ans4):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     
-    if u_ans4 == "Ballon":
+    if u_ans4 == "Balloon":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
